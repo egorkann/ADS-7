9da5f94b7247ccbaba4fa0913992c9de34d2e7cb
egorkann <minicraftnn@mail.ru>
2025-05-23 19:59:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,20 @@
 // Copyright 2025 NNTU-CS
 #include "train.h"
 
-#include <chrono>
 #include <iostream>
-#include <random>
 #include <string>
 #include <vector>
-
-using namespace std;
-
-void runExperiment(int n, const string& mode) {
-  Train train;
-  random_device rd;
-  mt19937 gen(rd());
-  uniform_int_distribution<> dis(0, 1);
-
-  for (int i = 0; i < n; ++i) {
-    if (mode == "on") {
-      train.addCar(true);
-    } else if (mode == "off") {
-      train.addCar(false);
-    } else {  // random
-      train.addCar(dis(gen));
-    }
-  }
-
-  auto start = chrono::high_resolution_clock::now();
-  int length = train.getLength();
-  auto end = chrono::high_resolution_clock::now();
-
-  chrono::duration<double> duration = end - start;
-
-  cout << "Mode: " << mode << ", n = " << n
-       << ", ops = " << train.getOpCount()
-       << ", time = " << duration.count() << " s, length = " << length << endl;
-}
+#include <algorithm>
+#include <cstdlib>
 
 int main() {
-  vector<int> sizes = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1000};
+  Train train;
+  train.addCar(true);
+  train.addCar(false);
+  train.addCar(true);
 
-  for (int n : sizes) {
-    runExperiment(n, "off");
-    runExperiment(n, "on");
-    runExperiment(n, "random");
-  }
+  std::cout << "Length: " << train.getLength() << std::endl;
+  std::cout << "Lights On: " << train.countLightsOn() << std::endl;
 
   return 0;
 }
